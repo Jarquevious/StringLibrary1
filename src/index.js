eadca8babc4235ea7e42d28eab7051c2f9d77fe6
Jarquevious <jarquevious@gmail.com>
2021-03-31 17:09:11 -0700

    snakecase added, kebob being fixed

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ function capitalize(str) {
   const word_without_first_letter = str.slice(1);
   return first_letter + word_without_first_letter;
 }
+console.log(' ')
+console.log("capitalize:")
 console.log("Before capitalize method: " + "jarquevious");
 console.log("After capitalize method: " + capitalize("jarquevious"));
 // ========================================================
@@ -33,7 +35,8 @@ console.log("After capitalize method: " + capitalize("jarquevious"));
 function allCaps(str) {
     return str.toUpperCase()
 }
-
+console.log(' ')
+console.log("allCaps:")
 console.log("Before allCaps method: " + "jarquevious");
 console.log("After allCaps method: " + allCaps("jarquevious"));
 
@@ -52,6 +55,8 @@ function capitalizeWords(str) {
   return firstUpperCaseLetter.join(' ')
 
 }
+console.log(' ')
+console.log("Capitalize words:")
 console.log(capitalizeWords('i am doing fine'))
 console.log(capitalizeWords('make schoool'))
 console.log(capitalizeWords('jarquevious nelson'))
@@ -65,6 +70,8 @@ function removeExtraSpaces(str)  {
   const filtered = chars.filter( (c) => c !== '')
   return result = filtered.join(' ')
 }
+console.log(' ')
+console.log("RemoveExtraSpace:")
 str = "   Hello    world!   "
 console.log(removeExtraSpaces(str))
 
@@ -77,13 +84,13 @@ console.log(removeExtraSpaces(str))
 // =======================================================
 // Challenge 4
 // =======================================================
-function kebobCase(str) {
+function kebobCase(str, seperator = '-') {
   // lower case
-  lowerCaseString = str.toLowerCase()
+  const lowerCaseString = str.toLowerCase()
   // split
-  eachlowerCS = lowerCaseString.split(' ')
+  const eachlowerCS = lowerCaseString.split(' ')
   // filter
-  filtered = eachlowerCS.filter( (c) => {
+  const filtered = eachlowerCS.filter( (c) => {
     const code = c.charCodeAt(0)
     if (code > 96 && code < 123) { // keep letters
       return true
@@ -91,19 +98,21 @@ function kebobCase(str) {
     } else if (code > 47 && code < 58) {// keep numbers
       return true 
 
-    } else if (code == 32 || code == 45) { // keep hyphen
+    } else if (code === 32 || code === seperator.charCodeAt(0)) { // keep hyphen
       return true
     }
     return false 
   })
   //remove extra spaces
-  const spaceFree = removeExtraSpaces(filtered.join(''))
+    const spaceFree = removeExtraSpaces(filtered.join(''))
 
   // split, join, return 
-  return spaceFree.split(' ').join('-')
+    return spaceFree.split(' ').join(' ')
 }
-str = 'Hello world'
-console.log( JSON.stringify(kebobCase(str)) )
+console.log(' ')
+console.log("KebabCase:")
+str = 'Hello world #$%^ a-hyphenated-word'
+console.log(kebobCase(str))
 // Convert the whole string to lower case with: string.toLowerCase()
 // Split the string into an array of characters with: string.split('')
 // Filter out the characters you don't want. You want to keep letters, numbers, the space ' ', and the hyphen '-'. One way to approach that is to use the character code. Every character is assigned a number (the character code) for lowercase letters the a through z are codes: 97 to 122. The space " " is character code 32. You can get the character code using string.charCodeAt(). To preserve the numbers look for character codes 48 to 57 (0 to 9). You're looking for character codes 32, 48-57, 97-122, and 45.
@@ -111,3 +120,13 @@ console.log( JSON.stringify(kebobCase(str)) )
 // Split on the space and join with hyphen.
 // Example: " Hello world " -> "hello-world"
 
+// =======================================================
+// Challenge 5
+// =======================================================
+function snakeCase(str) {
+  return kebobCase(str, '_')
+}
+console.log(' ')
+console.log("snakeCase:")
+str = 'Hello world #$%^ a-hyphenated-word'
+console.log(snakeCase(str))
